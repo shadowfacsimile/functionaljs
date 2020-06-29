65db94901d1ff9c8dc79eca3e7200f78de350fe8
shadowfacsimile <shadow.facsimile@gmail.com>
2020-06-29 18:11:35 +0530

    Added console output

--- combinators.js
+++ combinators.js
@@ -1,61 +1,61 @@
 const I = a => a; // I := λa.a  -- Idiot Bird Combinator --> Identity [id in Haskell]
-console.log(I(1));
-console.log(I(I));
+console.log(I(1)); // 1
+console.log(I(I)); // [Function: I]
 
 const M = f => f(f);    // M := λf.ff  -- Mocking Bird Combinator --> Self application
-console.log(M(I));
+console.log(M(I)); // [Function: I]
 
 const K = a => b => a;  // K := λab.a  -- Kestrel Combinator [const 5 6 = 5 in Haskell]
-console.log(K(I)(M));
+console.log(K(I)(M)); // [Function: I]
 
 // Why is named const in Haskell? Example below - a fixation to the number 5
-const K5 = K(5);
-console.log(K5);
-console.log(K5(2));
-console.log(K5(7));
-console.log(K5(2000));
+const K5 = K(5); // [Function]
+console.log(K5); // 5
+console.log(K5(2)); // 5
+console.log(K5(7)); // 5
+console.log(K5(2000)); // 5
 
 console.log(K(I)(2)(3)); // K(I)(x) --> I  --> K(I)(x)(y) = y  --> λab.b 
 
 const KI = a => b => b;  // KI := λab.b  -- Kite Combinator [const id 5 6 = 6 in Haskell]
-console.log(KI(I)(M));
+console.log(KI(I)(M)); // [Function: M]
 
 const C = f => a => b => f(b)(a); // C := λfab.fba  -- Cardinal Combinator --> Reverse arguments  [flip const 5 6 = 6 in Haskell]
-console.log(C(K)(I)(M));
+console.log(C(K)(I)(M)); // [Function: M]
 
 const T = K;
 const F = KI;
 T.inspect = () => 'T / K';
 F.inspect = () => 'F / KI';
-console.log(T);
-console.log(F);
+console.log(T); // T / K
+console.log(F); // F / KI
 const NOT = p => p(F)(T); // NOT := λp.pFT := C
-console.log(NOT(T));
-console.log(NOT(F));
-console.log(C(T));
+console.log(NOT(T)); // F / KI
+console.log(NOT(F)); // T / K
+console.log(C(T)); // [Function]
 
 const AND = p => q => p(q)(p); // AND := λpq.pqF := λpq.pqp
-console.log(AND(T)(F));
-console.log(AND(F)(F));
-console.log(AND(F)(T));
-console.log(AND(T)(T));
+console.log(AND(T)(F)); // F / KI
+console.log(AND(F)(F)); // F / KI
+console.log(AND(F)(T)); // F / KI
+console.log(AND(T)(T)); // T / K
 
 const OR = p => q => p(p)(q); // OR := λpq.pTq := λpq.ppq := M* [Mocking Bird once removed]
-console.log(OR(T)(F));
-console.log(OR(F)(F));
-console.log(OR(F)(T));
-console.log(OR(T)(T));
+console.log(OR(T)(F)); // T / K
+console.log(OR(F)(F)); // F / KI
+console.log(OR(F)(T)); // T / K
+console.log(OR(T)(T)); // T / K
 
-console.log(M(T)(F));
-console.log(M(F)(F));
-console.log(M(F)(T));
-console.log(M(T)(T));
+console.log(M(T)(F)); // T / K
+console.log(M(F)(F)); // F / KI
+console.log(M(F)(T)); // T / K
+console.log(M(T)(T)); // T / K
 
 const XOR = p => q => p(q)(NOT(q));
-console.log(XOR(T)(F));
-console.log(XOR(F)(F));
-console.log(XOR(F)(T));
-console.log(XOR(T)(T));
+console.log(XOR(T)(F)); // F / KI
+console.log(XOR(F)(F)); // T / K
+console.log(XOR(F)(T)); // F / KI
+console.log(XOR(T)(T)); // T / K
 
 const zero = f => a => a;
 const once = f => a => f(a);
@@ -64,7 +64,7 @@ const thrice = f => a => f(f(f(a)));
 const SUCC = n => f => a => f(n(f)(a));  // SUCC := λnfa.f(nfa) := λfab.B 
 const jsnum = n => n(x => x + 1)(0);
 
-console.log(jsnum(SUCC(zero)));
-console.log(jsnum(SUCC(once)));
-console.log(jsnum(SUCC(twice)));
-console.log(jsnum(SUCC(thrice)));
+console.log(jsnum(SUCC(zero))); // 1
+console.log(jsnum(SUCC(once))); // 2
+console.log(jsnum(SUCC(twice))); // 3
+console.log(jsnum(SUCC(thrice))); // 4
